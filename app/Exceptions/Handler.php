dcd8d4967db2cd703a4e413d6fb0db229d382b96
Munna Khan <ahkmunna13@gmail.com>
2018-12-03 15:55:12 +0600

    added .htaccess.exaple on the root

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
@@ -51,6 +52,12 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        // if ($exception instanceof ModelNotFoundException) {
+        //     if ($request->expectsJson()){
+        //         return response()->json(['error' => 'Resource not found.'], 404);
+        //     }
+        // }
+
         return parent::render($request, $exception);
     }
 
